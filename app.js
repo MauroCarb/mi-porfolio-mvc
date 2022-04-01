777e7d0084aea9002a0800a9359e87d878360899
MauroCarb <mauro.carbajales@gmail.com>
2022-03-31 22:17:26 -0300

    Practica patrones de diseño MVC terminada

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const mainRouter = require("./routers/main");
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("servidor funcionanando");
+app.listen(PORT, () => {
+  console.log(`servidor funcionanando en ${PORT}`);
 });
+
+app.use(express.static("public"));
+
+app.use("/", mainRouter);
